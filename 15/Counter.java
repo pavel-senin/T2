07662637e58b93b928ece3a865e0af3d6a64ce55
pavel-senin <120603369+pavel-senin@users.noreply.github.com>
2023-08-07 04:48:30 +0300

    Add files via upload
    15. Создайте класс Счетчик, у которого есть метод add(), увеличивающий̆ значение внутренней̆ int переменной̆ на 1 при нажатии “Завести новое животное” Сделайте так, чтобы с объектом такого типа можно было работать в блоке try-with-resources. Нужно бросить исключение, если работа с объектом типа счетчик была не в ресурсном try и/или ресурс остался открыт. Значение считать в ресурсе try, если при заведении животного заполнены все поля.

---
+++ 15/Counter.java
@@ -0,0 +1,16 @@
+public class Counter implements AutoCloseable {
+
+    static int sum;
+    {
+        sum = 0;
+    }
+
+    public void add() {
+        sum++;
+    }
+
+    @Override
+    public void close() {
+        System.out.println("Counter closed");
+    }
+}
